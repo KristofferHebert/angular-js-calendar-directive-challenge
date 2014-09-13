1d0285445c1f6366ddc4cf10dfaab6b6de237de9
hirekris <khebert@kristoffers-mbp.hsd1.wa.comcast.net>
2014-09-12 23:14:06 -0700

    added calendar challenge

--- app/app.js
+++ app/app.js
@@ -1,3 +1,80 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
+    .controller('mainCtrl', ['$scope',
+        function ($scope) {
 
-// your controller and directive code go here
+        }
+    ])
+    .directive('calendar', [
+
+        function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'calendar.html',
+                controller: function ($scope) {
+                    var today = new Date(),
+                        day = today.getDay(),
+                        year = today.getFullYear(),
+                        monthNumber = today.getMonth(),
+                        month;
+
+                    $scope.months = [
+                        'January',
+                        'February',
+                        'March',
+                        'April',
+                        'May',
+                        'June',
+                        'July',
+                        'August',
+                        'September',
+                        'October',
+                        'November',
+                        'December'
+                    ];
+
+                    $scope.days = [
+                        'Sunday',
+                        'Monday',
+                        'Tuesday',
+                        'Wednesday',
+                        'Thursday',
+                        'Friday',
+                        'Saturday',
+                    ];
+
+                    month = $scope.months[monthNumber];
+                    $scope.currentDate = {
+                        day: day,
+                        monthNumber: monthNumber,
+                        month: month,
+                        year: year,
+                    };
+
+                    $scope.years = function () {
+                        var beginning = $scope.currentDate.year - 20,
+                            end = beginning + 40,
+                            years = [];
+                        while (beginning <= end) {
+                            beginning++;
+                            years.push(beginning);
+                        }
+                        return years;
+                    };
+                    $scope.setRange = function () {
+                        parseInt($scope.currentDate.monthNumber, 10);
+                        return $scope.daysinmonth = CalendarRange.getMonthlyRange(new Date($scope.currentDate.year, $scope.currentDate.monthNumber));
+                    };
+                    $scope.setBg = function (value) {
+                        var cleanValue = parseInt(value, 10);
+                        return (cleanValue !== $scope.currentDate.monthNumber);
+                    };
+                    $scope.$watchCollection('currentDate', function () {
+                        $scope.setRange();
+
+                    });
+
+                    $scope.setRange();
+                }
+            };
+        }
+    ]);
